01fae705b7db55e738b7c9afdefc334de73c6388
939248060 <939248060@qq.com>
2019-04-20 10:39:47 +0800

    实验六更新

--- StudentJava/src/lianxi6/Lianxi01.java
+++ StudentJava/src/lianxi6/Lianxi01.java
@@ -7,25 +7,27 @@ public class Lianxi01 {
 
 	static int num1;
 	static int num2;
+
 	public static void main(String[] args) {
 
 		boolean flag = true;
-		while(flag) {
-		try {
-			inputNum();
-			flag = false;
-		} catch (Exception e) {
-			flag = true;
-			System.out.println("输入的整数有误，请重新输入两个整数");
-			continue;
+		while (flag) {
+			try {
+				inputNum();
+				flag = false;
+			} catch (Exception e) {
+				flag = true;
+				System.out.println("输入的整数有误，请重新输入两个整数");
+				continue;
+			}
 		}
-		}
-		
+
 	}
-	public static void inputNum() throws InputMismatchException{
+
+	public static void inputNum() throws InputMismatchException {
 		Scanner sc = new Scanner(System.in);
 		num1 = sc.nextInt();
 		num2 = sc.nextInt();
-		System.out.println("输入的整数为：num1="+num1+" num2="+num2);
+		System.out.println("输入的整数为：num1=" + num1 + " num2=" + num2);
 	}
 }
